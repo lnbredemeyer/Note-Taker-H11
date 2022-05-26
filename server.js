855427cfab12f6c7b8f0b4606039568a066248fe
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-05-25 20:05:53 -0500

    get route

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+
+// Initializes the app and creates a port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets up body parsing and static folder
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Creates a list of routes for Get, Post, Delete
+
+// Get Route
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        const parsedData = JSON.parse(data);
+        res.json(parsedData);
+      });
+});
+
+// Post Route
+
+// Delete Route
+
+app.listen(PORT, () => console.log(`Listening on Port: ${PORT}`));
